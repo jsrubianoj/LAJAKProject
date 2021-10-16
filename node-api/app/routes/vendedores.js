b842fa9a919f1ac5a28d997e4b82e89482764adf
Joha-G <jleidenschaft@gmail.com>
2021-10-16 11:52:26 -0500

    commit on main

--- node-api/app/routes/vendedores.js
+++ node-api/app/routes/vendedores.js
@@ -1,10 +1,13 @@
+const { getVendedor, DeleteVendedor, AddVendedor, UpdateVendedor } = require('../controllers/VendedorController')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send({
-        message: 'Vendedores'
-    })
-})
+router.get('/', getVendedor)
+
+router.post('/nuevaVenta', AddVendedor)
+
+router.delete('/eliminar/:id', DeleteVendedor)
+
+router.patch('/editar/:id', UpdateVendedor)
 
 module.exports = router 
