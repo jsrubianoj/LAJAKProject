5a3bbba21f38fa2155f3eddc6e69644eff5b0973
jromanosorio <jandres.roman.08@gmail.com>
2021-10-24 11:33:37 -0500

    configuracion base de datos en hosting

--- node-api/app/config/database.js
+++ node-api/app/config/database.js
@@ -1,5 +1,10 @@
 const {Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize('postgres://uetdnaggqgllca:78d30fcfbb073500648b938c1adf6f3ccaf3fd525b19f68c1915593a276e4b34@ec2-52-3-79-87.compute-1.amazonaws.com:5432/d8u50uu3sbcjoe')
+const sequelize = new Sequelize(
+    'sql5446415',
+    'sql5446415',
+    'LradUfq62S',
+    { host: 'sql5.freemysqlhosting.net', dialect: 'mysql'}
+)
 
 module.exports = sequelize
